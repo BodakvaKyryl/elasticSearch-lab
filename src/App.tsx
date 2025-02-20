511769d20ed09ff37a651571e3605835876b687c
Kyryl <bodakvakyryl@gmail.com>
2025-02-20 18:36:43 +0200

    feat: Implement search interface using Elastic Search UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,244 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ErrorBoundary, Facet, SearchBox, SearchProvider, WithSearch } from "@elastic/react-search-ui";
+import { Layout, Paging, PagingInfo, Results, SingleLinksFacet } from "@elastic/react-search-ui-views";
+import { SearchDriverOptions } from "@elastic/search-ui";
+import ElasticsearchAPIConnector, { SearchRequest } from "@elastic/search-ui-elasticsearch-connector";
+import moment from "moment";
+// import "./App.css";
+import { ResultData } from "./ResultData";
+import { Sport } from "./Sport";
 
-function App() {
-  const [count, setCount] = useState(0)
+const connector = new ElasticsearchAPIConnector(
+  {
+    host: "http://localhost:9200",
+    index: "sports",
+  },
+  (requestBody, requestState, queryConfig): SearchRequest => {
+    if (!requestState.searchTerm) return requestBody;
+
+    const searchFields = queryConfig.search_fields;
+
+    requestBody.query = {
+      multi_match: {
+        query: `${requestState.searchTerm}`,
+        fields: searchFields
+          ? Object.keys(searchFields).map((fieldName) => `${fieldName}^${searchFields[fieldName].weight || 1}`)
+          : undefined,
+        fuzziness: "AUTO",
+        prefix_length: 2,
+      },
+    };
+    return requestBody;
+  }
+);
+
+const config: SearchDriverOptions = {
+  apiConnector: connector,
+  debug: true,
+  alwaysSearchOnInitialLoad: true,
+  hasA11yNotifications: true,
+  searchQuery: {
+    search_fields: {
+      title: {},
+      categories: {},
+    },
+    result_fields: {
+      title: {
+        snippet: {
+          size: 100,
+          fallback: true,
+        },
+      },
+      event_date: {},
+      popularity: {},
+      sport_type: {},
+    },
+    disjunctiveFacets: ["event_date"],
+    facets: {
+      title_keyword: { type: "value", size: 10 },
+      popularity: {
+        type: "range",
+        ranges: [
+          { from: 1, name: "★☆☆☆☆ & Up" },
+          { from: 2, name: "★★☆☆☆ & Up" },
+          { from: 3, name: "★★★☆☆ & Up" },
+          { from: 4, name: "★★★★☆ & Up" },
+          { from: 5, name: "★★★★★" },
+        ],
+      },
+      event_date: {
+        type: "range",
+        ranges: [
+          {
+            from: moment().subtract(30, "years").toISOString(),
+            name: "Within the last 30 years",
+          },
+          {
+            from: moment().subtract(30, "years").toISOString(),
+            to: moment().subtract(70, "years").toISOString(),
+            name: "30 - 70 years ago",
+          },
+          {
+            to: moment().subtract(70, "years").toISOString(),
+            name: "More than 70 years ago",
+          },
+        ],
+      },
+    },
+  },
+  autocompleteQuery: {
+    results: {
+      resultsPerPage: 5,
+      result_fields: {
+        title: {
+          snippet: {
+            size: 100,
+            fallback: true,
+          },
+        },
+        event_date: {},
+        popularity: {},
+        sport_type: {},
+      },
+    },
+  },
+};
+
+function App(): JSX.Element {
+  const sport: Sport = {
+    title: "",
+    event_date: "",
+    popularity: 0,
+    sport_type: [],
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <SearchProvider config={config}>
+      <WithSearch
+        mapContextToProps={({ result, setPage }) => ({
+          result,
+          setPage,
+        })}>
+        {({ setPage }): JSX.Element => {
+          return (
+            <div className="App">
+              <ErrorBoundary>
+                <Layout
+                  header={
+                    <SearchBox
+                      autocompleteMinimumCharacters={3}
+                      autocompleteResults={{
+                        linkTarget: "_blank",
+                        sectionTitle: "Results",
+                        titleField: "title",
+                        urlField: "any",
+                        shouldTrackClickThrough: true,
+                        clickThroughTags: ["test"],
+                      }}
+                      autocompleteSuggestions={true}
+                      debounceLength={0}
+                    />
+                  }
+                  sideContent={
+                    <div>
+                      <Facet field="popularity" label="Popularity" view={SingleLinksFacet} />
+                      <Facet field="event_date" label="Date" isFilterable={true} filterType="any" />
+                      <div className="sui-facet">
+                        <legend className="sui-facet__title">Add Sport</legend>
+                        <div className="sui-facet-search">
+                          <input
+                            className="sui-facet-search__text-input"
+                            type="date"
+                            onChange={(evt): string => (sport.event_date = `${evt.target.value}`)}
+                          />
+                          <input
+                            className="sui-facet-search__text-input"
+                            type="text"
+                            placeholder="Title"
+                            onChange={(evt): string => (sport.title = evt.target.value)}
+                          />
+                          <input
+                            className="sui-facet-search__text-input"
+                            type="text"
+                            placeholder="Sport Type"
+                            onChange={(evt): string[] => (sport.sport_type = evt.target.value.split(", "))}
+                          />
+                          <input
+                            className="sui-facet-search__text-input"
+                            type="number"
+                            max="5"
+                            min="0"
+                            placeholder="Rate"
+                            onChange={(evt): number => (sport.popularity = Number(evt.target.value))}
+                          />
+                          <button
+                            onClick={async (): Promise<void> => {
+                              await fetch("http://localhost:9200/sports/_doc", {
+                                method: "POST",
+                                headers: {
+                                  "Content-Type": "application/json",
+                                },
+                                body: JSON.stringify({ ...sport }),
+                              });
+                              window.location.reload();
+                            }}>
+                            Add
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  }
+                  bodyContent={
+                    <Results
+                      titleField="title"
+                      resultView={({ result }: { result: ResultData }): JSX.Element => (
+                        <li className="sui-result">
+                          <div className="sui-result__header">
+                            <h3>
+                              <a
+                                dangerouslySetInnerHTML={{
+                                  __html: result.title.raw,
+                                }}
+                                target="_blank"
+                              />
+                            </h3>
+                          </div>
+                          <div className="sui-result__body">
+                            <div className="sui-result__details">
+                              <div>
+                                {result.event_date.raw}
+                                <br />
+                                {"Popularity Score: "}
+                                {result.popularity_score.raw}
+                                <br />
+                                {result.categories.raw.join(", ")}
+                                <br />
+                                <button
+                                  onClick={async (): Promise<void> => {
+                                    await fetch("http://localhost:9200/sports/_doc/" + result.id.raw, {
+                                      method: "DELETE",
+                                    });
+                                    window.location.reload();
+                                  }}>
+                                  Delete
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      )}
+                      shouldTrackClickThrough={true}
+                    />
+                  }
+                  bodyHeader={<PagingInfo start={0} end={0} searchTerm={""} totalResults={0} />}
+                  bodyFooter={<Paging totalPages={0} onChange={setPage} />}
+                />
+              </ErrorBoundary>
+            </div>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
+  );
 }
 
-export default App
+export default App;
