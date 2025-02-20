31b3635759722abe576d7621403d2395ead47a15
Kyryl <bodakvakyryl@gmail.com>
2025-02-20 18:33:56 +0200

    refactor: Add Sport and ResultData interfaces with relevant properties

---
+++ src/ResultData.ts
@@ -0,0 +1,17 @@
+export interface ResultData {
+  title: {
+    raw: string;
+  };
+  event_date: {
+    raw: string;
+  };
+  popularity_score: {
+    raw: number;
+  };
+  categories: {
+    raw: string[];
+  };
+  id: {
+    raw: string;
+  };
+}
---
+++ src/Sport.ts
@@ -0,0 +1,6 @@
+export interface Sport {
+  title: string;
+  event_date: string;
+  popularity: number;
+  sport_type: string[];
+}
